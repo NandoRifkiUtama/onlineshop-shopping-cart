bd328ffd3db94972a927e4e05672b015f3216218
NandoRifki <nando.rifki.utama@gmail.com>
2023-03-05 12:38:25 +0700

    final commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const products = require("./products")
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req,res)=>{
+    res.send("welcome to our API service...")
+})
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`server is running on port ${port}`));
---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: 1,
+    name: "Galaxy S22 Ultra",
+    description: "6.8-inch Edge QHD+",
+    price: 954,
+    image:
+      "https://www.samsungmobilepress.com/file/A20AFBC5AD503566965D392E91618742CB5DA27E14BFBBBADB87121B0A7ADA2078CC174B3F38A9FF96D50E1755011F979945C4E8CC725F605687D548951D40042242598D49AD0FD2FE86B85085CC6B6B90A646A444171535D60D76DF97C05E7A48C83E1E1A8078C5409B9376961B65778895555E5F9B8419B4A818F4E3E68C78B656E8256E34DA4C010E876A0E95755657DB1692B07772FA731AD9A0A333FB1F",
+  },
+  {
+    id: 2,
+    name: "iPhone 14 Pro",
+    description: "Max Deep Purple",
+    price: 999,
+    image:
+      "https://cdn.eraspace.com/media/catalog/product/i/p/iphone_14_pro_max_deep_purple_1_6.jpg",
+  },
+  {
+    id: 3,
+    name: "Xiaomi 13 Pro",
+    description: "5G Dual SIM with Leica Optics",
+    price: 720,
+    image:
+      "https://i.expansys.net/img/b/393196/xiaomi-13-pro-5g-dual-sim-with-leica-optics.jpg",
+  },
+  {
+    id: 4,
+    name: "iPhone 13 Pro Max",
+    description: "6.7 inci, 2778 x 1284 pixels",
+    price: 945,
+    image: "https://store.ite.net/wp-content/uploads/2021/10/iPhone_13_Pro_Max_Sierra_Blue_PDP_Image_position-2__en-US.png",
+  },
+];
+
+module.exports = products
